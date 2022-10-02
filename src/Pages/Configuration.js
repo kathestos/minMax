5b7a0a5ea22af4810928aa43b2a670d6256b1d4b
kathestos <mihael.fucek@gmail.com>
2022-10-02 15:54:00 +0200

    translation fix try

--- src/Pages/Configuration.js
+++ src/Pages/Configuration.js
@@ -17,7 +17,7 @@ i18n
       caches: ["cookie"],
     },
     backend: {
-      loadPath: "minMax/assets/locales/{{lng}}/translation.json",
+      loadPath: "assets/locales/{{lng}}/translation.json",
     },
   });
 
